75814b64f22594ebf7d2d225169906a49b1fa341
Javier1609 <jvrgmz@myuax.com>
2023-12-18 18:21:14 +0100

    .

---
+++ ejercicio_1.cpp
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+ int main () {
+    int numero_1 = -3;
+    int numero_2 = -2;
+
+    int numero_1_absoluto = abs(numero_1);
+    int numero_2_absoluto = abs(numero_2);
+
+    printf("El valor absoluto de %d es %d\n", numero_1, numero_1_absoluto);
+    printf("El valor absoluto de %d es %d\n", numero_2, numero_2_absoluto);
+
+};
